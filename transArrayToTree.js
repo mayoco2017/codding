77475fb39d63495c6504ce214362e96948e1036d
lj <1093876183@qq.com>
2022-11-01 12:50:39 +0800

    feat: update transArrayToTree

--- transArrayToTree.js
+++ transArrayToTree.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-10-31 16:56:46
  * @LastEditors: lj 1093876183@qq.com
- * @LastEditTime: 2022-10-31 18:19:20
+ * @LastEditTime: 2022-11-01 12:49:06
  * @FilePath: /codding/transArrayToTree.js
  * @Description: 一维数组转树结构
  */
@@ -18,6 +18,7 @@ function transArrayToTree1(array) {
   const arrayToTree = (tree, list, pid) => {
     list.forEach((item) => {
       if (item.pid === pid) {
+        //创建children，递归调arrayToTree，给arrayToTree加入满足条件的子元素
         const child = {
           ...item,
           children: [],
@@ -35,6 +36,25 @@ function transArrayToTree1(array) {
   return resultTree;
 }
 
+function transArrayToTree2(array){
+    //创建一个id：item映射
+    //遍历list将符合条件的项放入映射
+    const idItemMap = {}
+    array.forEach((item)=>{
+        idItemMap[item.id] = item
+        idItemMap["root"] = {}
+    })
+    array.forEach((item)=>{
+        if(idItemMap[item.pid||"root"].children){
+            idItemMap[item.pid||"root"].children.push(item) 
+        }else{
+            idItemMap[item.pid||"root"].children = [item]
+        }
+        
+    })
+    return idItemMap["root"].children
+}
+
 const testList = [
   { id: 1, name: "根元素", pid: null },
   { id: 2, name: "子元素2", pid: 1 },
@@ -46,3 +66,4 @@ const testList = [
 ];
 
 console.log("this is answer1", transArrayToTree1(testList));
+console.log("this is answer2", transArrayToTree2(testList));
